9e1f2d816f92ede8b1a16ac11a8315bfea2a7e3b
rabah babaci <4rabah@gmail.com>
2020-06-20 14:56:19 -0700

    add a timestamp column to users-products

--- src/db/mockData/usersProducts.js
+++ src/db/mockData/usersProducts.js
@@ -1,12 +1,14 @@
+const now = require("../../utils/getLocalDateTime");
+
 module.exports = [
-  { user_id: 75, product_id: 43 },
-  { user_id: 76, product_id: 2 },
-  { user_id: 42, product_id: 44 },
-  { user_id: 30, product_id: 43 },
-  { user_id: 66, product_id: 18 },
-  { user_id: 79, product_id: 87 },
-  { user_id: 29, product_id: 62 },
-  { user_id: 85, product_id: 68 },
-  { user_id: 54, product_id: 89 },
-  { user_id: 52, product_id: 17 },
+  { user_id: 75, product_id: 43, created_at: now(), updated_at: now() },
+  { user_id: 76, product_id: 2, created_at: now(), updated_at: now() },
+  { user_id: 42, product_id: 44, created_at: now(), updated_at: now() },
+  { user_id: 30, product_id: 43, created_at: now(), updated_at: now() },
+  { user_id: 66, product_id: 18, created_at: now(), updated_at: now() },
+  { user_id: 79, product_id: 87, created_at: now(), updated_at: now() },
+  { user_id: 29, product_id: 62, created_at: now(), updated_at: now() },
+  { user_id: 85, product_id: 68, created_at: now(), updated_at: now() },
+  { user_id: 54, product_id: 89, created_at: now(), updated_at: now() },
+  { user_id: 52, product_id: 17, created_at: now(), updated_at: now() },
 ];
