ed11a3bdd8b1597b36679c78ef7bf14c040f6452
rabah babaci <4rabah@gmail.com>
2020-06-19 18:40:03 -0700

    create the users migration

--- src/db/migrations/20200619174721_create-users.js
+++ src/db/migrations/20200619174721_create-users.js
@@ -1,8 +1,19 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("users", table => {
+    table.increments();
+    table.string("first_name", 64).notNullable();
+    table.string("last_name", 64).notNullable();
+    table.string("email", 64).notNullable().unique();
+    table.string("username", 64).notNullable().unique();
+    table.string("password", 64).notNullable();
+    table.date("date_of_birth").notNullable();
+    table
+      .timestamp("registered_at", { precision: 6 })
+      .defaultTO(knex.fn.now(6));
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.dropTableIfExists("users");
 };
