03cbd6cb167d8f9525db557d9bd7c169be324200
rabah babaci <4rabah@gmail.com>
2020-06-20 09:45:24 -0700

    create the cleanup and users seeds

--- src/db/seeds/01-users.js
+++ src/db/seeds/01-users.js
@@ -1,7 +1,9 @@
+const fakeUsers = require("../mockData/users");
+
 exports.seed = function (knex) {
   return knex("users")
     .del()
     .then(function () {
-      return knex("users").insert([]);
+      return knex("users").insert(fakeUsers);
     });
 };
