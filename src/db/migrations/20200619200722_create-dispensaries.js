5d2db8f96ccce15c40baa858d8f5950f0909313f
rabah babaci <4rabah@gmail.com>
2020-06-20 14:21:01 -0700

    import the dispensaries dummy data

--- src/db/migrations/20200619200722_create-dispensaries.js
+++ src/db/migrations/20200619200722_create-dispensaries.js
@@ -1,16 +1,14 @@
 exports.up = function (knex) {
   return knex.schema.createTable("dispensaries", table => {
     table.increments();
-    table.string("name", 64).notNullable();
-    table.string("address", 64).notNullable();
-    table.string("city", 64).notNullable().unique();
-    table.string("state", 64).notNullable().unique();
+    table.string("name", 64).notNullable().unique();
+    table.string("address", 64).notNullable().unique();
+    table.string("city", 64).notNullable();
+    table.string("state", 64).notNullable();
     table.string("postal_code", 64).notNullable();
-    table.string("country", 64).notNullable();
     table.string("phone_number", 64).notNullable();
-    table.string("email", 64).notNullable();
+    table.string("email", 64).notNullable().unique();
     table.string("logo_url", 64).notNullable();
-    table.string("page_url", 64).notNullable();
     table.boolean("has_delivery").notNullable();
     table.string("created_at", 64).notNullable();
   });
