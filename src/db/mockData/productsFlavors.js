8913fbe0ade0429ff6702eb8eafb5ee5db36ecf6
rabah babaci <4rabah@gmail.com>
2020-06-20 15:21:31 -0700

    create the products_flavors seed

---
+++ src/db/mockData/productsFlavors.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    product_id: 46,
+    flavor_id: 33,
+  },
+  {
+    product_id: 6,
+    flavor_id: 16,
+  },
+  {
+    product_id: 51,
+    flavor_id: 57,
+  },
+  {
+    product_id: 11,
+    flavor_id: 40,
+  },
+  {
+    product_id: 53,
+    flavor_id: 20,
+  },
+  {
+    product_id: 89,
+    flavor_id: 88,
+  },
+  {
+    product_id: 81,
+    flavor_id: 1,
+  },
+  {
+    product_id: 11,
+    flavor_id: 25,
+  },
+  {
+    product_id: 97,
+    flavor_id: 39,
+  },
+  {
+    product_id: 33,
+    flavor_id: 7,
+  },
+];
