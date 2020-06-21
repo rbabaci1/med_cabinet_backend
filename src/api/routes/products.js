699d407f42e4a9d8c3a4925b58c15a1fa8d0986b
rabah babaci <4rabah@gmail.com>
2020-06-21 11:15:11 -0700

    define getProductById route

--- src/api/routes/products.js
+++ src/api/routes/products.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 const controller = require("../controllers/productsGet");
 
 router.get("/", controller.getProducts);
+router.get("/:id", controller.getProductById);
 
 module.exports = router;
