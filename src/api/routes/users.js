e2c1c30b88a3e7b3eba933eebfc648c027cf74a1
rabah babaci <4rabah@gmail.com>
2020-06-21 12:53:10 -0700

    use a variable when call validateId instead of a string

--- src/api/routes/users.js
+++ src/api/routes/users.js
@@ -2,10 +2,11 @@ const router = require("express").Router();
 
 const controller = require("../controllers/usersGet");
 const { validateId } = require("../middlewares/global");
+const TABLE_NAME = "users";
 
 router.get("/", controller.getUsers);
-router.get("/:id", controller.getUserById);
-router.get("/:id/fullInfo", validateId("users"), controller.getUserFullInfo);
+router.get("/:id", validateId(TABLE_NAME), controller.getUserById);
+router.get("/:id/fullInfo", validateId(TABLE_NAME), controller.getUserFullInfo);
 // router.get("/:id/products", validateId("users"), controller.getUserProducts);
 // router.get("/:id/reviews", validateId("users"), controller.getUserReviews);
 
