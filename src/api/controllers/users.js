2e379e971730bb3d405ed51ccd03d146ef5f91d2
rabah babaci <4rabah@gmail.com>
2020-06-20 23:42:39 -0700

    create the get user by id endpoint

--- src/api/controllers/users.js
+++ src/api/controllers/users.js
@@ -1,4 +1,4 @@
-const { getAll } = require("../../db/models/allRoutes");
+const { getAll, getBy } = require("../../db/models/allRoutes");
 
 const getUsers = async (req, res) => {
   try {
@@ -11,4 +11,16 @@ const getUsers = async (req, res) => {
   }
 };
 
-module.exports = { getUsers };
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await getBy("users", { id });
+    res.status(200).json({ user });
+  } catch ({ message }) {
+    res
+      .status(500)
+      .json({ message: "Could not retrieve user now.", reason: message });
+  }
+};
+
+module.exports = { getUsers, getUserById };
--- src/db/models/allRoutes.js
+++ src/db/models/allRoutes.js
@@ -1,6 +1,6 @@
 const db = require("../dbConfig");
 
 const getAll = tableName => db(tableName);
-const getBy = (tableName, filter) => db(tableName).where(filter);
+const getBy = (tableName, filter) => db(tableName).where(filter).first();
 
 module.exports = { getAll, getBy };
