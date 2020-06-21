d07e098976a8b9370c29b2f161ea9302d1352427
rabah babaci <4rabah@gmail.com>
2020-06-21 10:13:03 -0700

    define the route to get user products

--- src/api/routers/users.js
+++ src/api/routers/users.js
@@ -1,9 +1,14 @@
 const router = require("express").Router();
 
-const { getUsers, getUserById } = require("../controllers/users");
+const {
+  getUsers,
+  getUserById,
+  getUserProducts,
+} = require("../controllers/users");
 const { validateId } = require("../middlewares/global");
 
 router.get("/", getUsers);
 router.get("/:id", validateId("users"), getUserById);
+router.get("/:id/products", validateId("users"), getUserProducts);
 
 module.exports = router;
