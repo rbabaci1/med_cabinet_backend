73bce6b9578e61ddaed8b7acf4e0ae4e98dbcc41
rabah babaci <4rabah@gmail.com>
2020-06-20 21:05:29 -0700

    comment all routes

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ server.use(express.json());
 server.use(helmet());
 server.use(cors());
 
-server.use("/api/users", usersRouter);
+// server.use("/api/users", usersRouter);
 // server.use("/api/products", productsRouter);
 // server.use("/api/dispensaries", dispensariesRouter);
 
