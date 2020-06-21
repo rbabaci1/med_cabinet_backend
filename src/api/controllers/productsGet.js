7676f6ffc0d70ed3a8595e17eaff158098b07648
rabah babaci <4rabah@gmail.com>
2020-06-21 11:12:15 -0700

    create the getProducts endpoint

--- src/api/controllers/productsGet.js
+++ src/api/controllers/productsGet.js
@@ -1,5 +1,15 @@
 const { getAll } = require("../../db/models/global");
 
-const getProducts = async (req, res) => {};
+const getProducts = async (req, res) => {
+  try {
+    const products = await getAll("products");
+    res.status(200).json(products);
+  } catch ({ message }) {
+    res.status(500).json({
+      message: "The products list can't be retrieved at this moment",
+      reason: message,
+    });
+  }
+};
 
 module.exports = { getProducts };
