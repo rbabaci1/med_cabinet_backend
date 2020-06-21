b09659add864f191ec62834ce0ae95aec0460041
rabah babaci <4rabah@gmail.com>
2020-06-21 11:16:27 -0700

    add the validateId to get products by id enpoint

--- src/api/routes/products.js
+++ src/api/routes/products.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 
 const controller = require("../controllers/productsGet");
+const { validateId } = require("../middlewares/global");
 
 router.get("/", controller.getProducts);
-router.get("/:id", controller.getProductById);
+router.get("/:id", validateId("products"), controller.getProductById);
 
 module.exports = router;
