f8afcc3c1a4777ea2ab23e74ed9ca9fba2d6fcb3
rabah babaci <4rabah@gmail.com>
2020-06-20 23:26:58 -0700

    create the get users controller

--- src/api/controllers/users.js
+++ src/api/controllers/users.js
@@ -1,14 +1,12 @@
 const { getAll } = require("../../db/models/allRoutes");
 
-const router = require("express").Router();
-
-router.get("/", async (req, res) => {
+const get = async (req, res, next) => {
   try {
     const users = await getAll("users");
     res.status(200).json(users);
   } catch (error) {
     console.error(error);
   }
-});
+};
 
-module.exports = router;
+module.exports = { get };
