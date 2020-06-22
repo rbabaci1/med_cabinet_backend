265d724d7177f39b502d750300fa486ea0dd9066
rabah babaci <4rabah@gmail.com>
2020-06-21 18:11:33 -0700

    define the get flavorById req handler

--- src/api/routes/flavors.js
+++ src/api/routes/flavors.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 const controller = require("../controllers/flavorsGet");
 
 router.get("/", controller.getFlavors);
+router.get("/:id", controller.getFlavorById);
 
 module.exports = router;
