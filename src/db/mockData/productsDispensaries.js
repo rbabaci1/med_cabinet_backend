64e036b1cc59622d9acc4c5848b6dc00b61dbab3
rabah babaci <4rabah@gmail.com>
2020-06-21 19:45:15 -0700

    reset database

--- src/db/mockData/productsDispensaries.js
+++ src/db/mockData/productsDispensaries.js
@@ -1,83 +1,403 @@
 let productsDispensaries = [
   {
-    product_id: 85,
+    product_id: 1,
+    dispensary_id: 5,
+  },
+  {
+    product_id: 2,
+    dispensary_id: 5,
+  },
+  {
+    product_id: 3,
     dispensary_id: 4,
   },
   {
-    product_id: 50,
+    product_id: 4,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 5,
     dispensary_id: 5,
   },
   {
-    product_id: 42,
+    product_id: 6,
+    dispensary_id: 4,
+  },
+  {
+    product_id: 7,
     dispensary_id: 5,
+  },
+  {
+    product_id: 8,
+    dispensary_id: 5,
+  },
+  {
+    product_id: 9,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 10,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 11,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 12,
+    dispensary_id: 2,
+  },
+  {
+    product_id: 13,
+    dispensary_id: 4,
+  },
+  {
+    product_id: 14,
+    dispensary_id: 4,
+  },
+  {
+    product_id: 15,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 16,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 17,
+    dispensary_id: 5,
+  },
+  {
+    product_id: 18,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 19,
+    dispensary_id: 4,
+  },
+  {
+    product_id: 20,
+    dispensary_id: 2,
+  },
+  {
+    product_id: 21,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 22,
+    dispensary_id: 2,
+  },
+  {
+    product_id: 23,
+    dispensary_id: 2,
+  },
+  {
+    product_id: 24,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 25,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 26,
+    dispensary_id: 4,
+  },
+  {
+    product_id: 27,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 28,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 29,
+    dispensary_id: 2,
+  },
+  {
+    product_id: 30,
+    dispensary_id: 2,
+  },
+  {
+    product_id: 31,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 32,
+    dispensary_id: 5,
+  },
+  {
+    product_id: 33,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 34,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 35,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 36,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 37,
+    dispensary_id: 2,
+  },
+  {
+    product_id: 38,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 39,
+    dispensary_id: 5,
+  },
+  {
+    product_id: 40,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 41,
+    dispensary_id: 2,
+  },
+  {
+    product_id: 42,
+    dispensary_id: 4,
+  },
+  {
+    product_id: 43,
+    dispensary_id: 4,
+  },
+  {
+    product_id: 44,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 45,
+    dispensary_id: 2,
+  },
+  {
+    product_id: 46,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 47,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 48,
+    dispensary_id: 2,
+  },
+  {
+    product_id: 49,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 50,
+    dispensary_id: 2,
+  },
+  {
+    product_id: 51,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 52,
+    dispensary_id: 2,
+  },
+  {
+    product_id: 53,
+    dispensary_id: 2,
+  },
+  {
+    product_id: 54,
+    dispensary_id: 1,
   },
   {
     product_id: 55,
     dispensary_id: 4,
   },
   {
-    product_id: 60,
-    dispensary_id: 4,
-  },
-  {
-    product_id: 6,
+    product_id: 56,
     dispensary_id: 1,
   },
   {
-    product_id: 100,
+    product_id: 57,
+    dispensary_id: 5,
+  },
+  {
+    product_id: 58,
+    dispensary_id: 2,
+  },
+  {
+    product_id: 59,
+    dispensary_id: 2,
+  },
+  {
+    product_id: 60,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 61,
     dispensary_id: 4,
   },
   {
-    product_id: 89,
+    product_id: 62,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 63,
+    dispensary_id: 2,
+  },
+  {
+    product_id: 64,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 65,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 66,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 67,
+    dispensary_id: 5,
+  },
+  {
+    product_id: 68,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 69,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 70,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 71,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 72,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 73,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 74,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 75,
+    dispensary_id: 4,
+  },
+  {
+    product_id: 76,
     dispensary_id: 2,
   },
   {
     product_id: 77,
-    dispensary_id: 2,
+    dispensary_id: 3,
   },
   {
-    product_id: 73,
+    product_id: 78,
     dispensary_id: 4,
   },
   {
-    product_id: 29,
+    product_id: 79,
     dispensary_id: 3,
   },
   {
-    product_id: 55,
+    product_id: 80,
     dispensary_id: 2,
   },
   {
-    product_id: 36,
+    product_id: 81,
     dispensary_id: 1,
   },
   {
-    product_id: 6,
+    product_id: 82,
+    dispensary_id: 4,
+  },
+  {
+    product_id: 83,
+    dispensary_id: 4,
+  },
+  {
+    product_id: 84,
+    dispensary_id: 1,
+  },
+  {
+    product_id: 85,
     dispensary_id: 3,
+  },
+  {
+    product_id: 86,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 87,
+    dispensary_id: 5,
+  },
+  {
+    product_id: 88,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 89,
+    dispensary_id: 4,
   },
   {
     product_id: 90,
     dispensary_id: 5,
   },
   {
-    product_id: 2,
-    dispensary_id: 2,
+    product_id: 91,
+    dispensary_id: 5,
   },
   {
-    product_id: 57,
+    product_id: 92,
     dispensary_id: 4,
   },
   {
-    product_id: 82,
+    product_id: 93,
+    dispensary_id: 4,
+  },
+  {
+    product_id: 94,
     dispensary_id: 2,
   },
   {
-    product_id: 19,
+    product_id: 95,
+    dispensary_id: 4,
+  },
+  {
+    product_id: 96,
     dispensary_id: 3,
   },
   {
-    product_id: 44,
+    product_id: 97,
+    dispensary_id: 2,
+  },
+  {
+    product_id: 98,
     dispensary_id: 1,
+  },
+  {
+    product_id: 99,
+    dispensary_id: 3,
+  },
+  {
+    product_id: 100,
+    dispensary_id: 2,
   },
 ];
 
