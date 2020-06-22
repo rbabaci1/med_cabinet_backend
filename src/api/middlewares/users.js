cf90e1b71415a535b5a0f5289f199ccedc115244
rabah babaci <4rabah@gmail.com>
2020-06-22 15:21:49 -0700

    create the Joi user validation schema

--- src/api/middlewares/users.js
+++ src/api/middlewares/users.js
@@ -1,5 +1,12 @@
-// const Joi = require("@hapi/joi");
+const Joi = require("@hapi/joi");
 
-// const userSchema = Joi.object({
-//     firstN
-// })
+const userSchema = Joi.object({
+  firstName: Joi.string().min(3).required(),
+  lastName: Joi.string().min(3).required(),
+  email: Joi.string().email(),
+  username: Joi.string().alphanum().min(3).required(),
+  password: Joi.string().required(),
+  created_at: Joi.string().pattern(new RegExp("d{4}-d{2}-d{2} d{2}:d{2}:d{2}")),
+});
+
+module.exports = userSchema;
