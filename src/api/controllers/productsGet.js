edc2cd4bc8cf5dde408bc3786f897d468107f3ae
rabah babaci <4rabah@gmail.com>
2020-06-22 12:43:10 -0700

    define the get product provider route

--- src/api/controllers/productsGet.js
+++ src/api/controllers/productsGet.js
@@ -15,21 +15,25 @@ const getProducts = async (req, res) => {
   }
 };
 
-const getProductById = async (req, res) => {
-  try {
-    const { product, params } = req;
-    const [provider = false] = await Product.getProvider(product.dispensary_id);
-    const [ratings = false] = await Product.getRatings(params.id);
-    const flavors = await Product.getFlavors(params.id);
-    const effects = await Product.getEffects(params.id);
-
-    res.status(200).json({ ...product, provider, flavors, effects, ratings });
-  } catch ([message]) {
-    res.status(500).json({
-      message: "The product details can't be retrieved at this moment",
-      reason: message,
-    });
-  }
+const getProductById = (req, res) => {
+  res.status(200).json(req.product);
 };
+
+// const getProductById = async (req, res) => {
+//   try {
+//     const { product, params } = req;
+//     const [provider = false] = await Product.getProvider(product.dispensary_id);
+//     const [ratings = false] = await Product.getRatings(params.id);
+//     const flavors = await Product.getFlavors(params.id);
+//     const effects = await Product.getEffects(params.id);
+
+//     res.status(200).json({ ...product, provider, flavors, effects, ratings });
+//   } catch ([message]) {
+//     res.status(500).json({
+//       message: "The product details can't be retrieved at this moment",
+//       reason: message,
+//     });
+//   }
+// };
 
 module.exports = { getProducts, getProductById };
--- src/api/routes/products.js
+++ src/api/routes/products.js
@@ -6,5 +6,10 @@ const TABLE_NAME = "products";
 
 router.get("/", controller.getProducts);
 router.get("/:id", validateId(TABLE_NAME), controller.getProductById);
+router.get(
+  "/:id/provider",
+  validateId(TABLE_NAME),
+  controller.getProductProvider
+);
 
 module.exports = router;
