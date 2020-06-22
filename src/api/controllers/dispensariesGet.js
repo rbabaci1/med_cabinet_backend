8125dc37a579ab6a5b9ff9e01ad6be904d123fa4
rabah babaci <4rabah@gmail.com>
2020-06-21 19:14:16 -0700

    create the getDispensaryProduct db model

--- src/api/controllers/dispensariesGet.js
+++ src/api/controllers/dispensariesGet.js
@@ -1,4 +1,5 @@
 const { getAll } = require("../../db/models/global.js");
+const { getDispensaryHours } = require("../../db/models/dispensaries");
 
 const TABLE_NAME = "dispensaries";
 
@@ -14,8 +15,19 @@ const getDispensaries = async (req, res) => {
   }
 };
 
-const getDispensaryById = async (req, res) => {
-  res.status(200).json(req.dispensary);
+const getDispensaryFullInfo = async (req, res) => {
+  try {
+    const { dispensary } = req.dispensary;
+    const business_hours = await getDispensaryHours(dispensary.id);
+    // const dispensaryProducts = await getDispensaryProducts(id)
+
+    res.status(200).json({ ...dispensary, business_hours });
+  } catch ({ message }) {
+    res.status(500).json({
+      message: "The dispensary details can't be retrieved at this moment",
+      reason: message,
+    });
+  }
 };
 
-module.exports = { getDispensaries, getDispensaryById };
+module.exports = { getDispensaries, getDispensaryFullInfo };
--- src/api/routes/dispensaries.js
+++ src/api/routes/dispensaries.js
@@ -6,6 +6,6 @@ const { validateId } = require("../middlewares/global");
 const TABLE_NAME = "dispensaries";
 
 router.get("/", controller.getDispensaries);
-router.get("/:id", validateId(TABLE_NAME), controller.getDispensaryById);
+router.get("/:id", validateId(TABLE_NAME), controller.getDispensaryFullInfo);
 
 module.exports = router;
