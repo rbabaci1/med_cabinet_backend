aaedd1ed07c86718cdd323e83283544ff98147bd
rabah babaci <4rabah@gmail.com>
2020-06-21 18:08:03 -0700

    create the getFlavors request handler

---
+++ src/api/controllers/flavorsGet.js
@@ -0,0 +1,17 @@
+const { getAll } = require("../../db/models/global.js");
+
+const TABLE_NAME = "flavors";
+
+const getFlavors = async (req, res) => {
+  try {
+    const flavors = await getAll(TABLE_NAME);
+    res.status(200).json(flavors);
+  } catch ({ message }) {
+    res.status(500).json({
+      message: "The flavors list can't be retrieved at this moment",
+      reason: message,
+    });
+  }
+};
+
+module.exports = { getFlavors };
