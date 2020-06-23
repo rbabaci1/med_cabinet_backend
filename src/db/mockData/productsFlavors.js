8df7dd64fb2d16ba066c5da3cdb1526c27628ee0
rabah babaci <4rabah@gmail.com>
2020-06-22 23:56:48 -0700

    insert all the products flavors pairs

--- src/db/mockData/productsFlavors.js
+++ src/db/mockData/productsFlavors.js
@@ -1,26 +1,9 @@
-module.exports = [
-  {
-    product_id: 1,
-    flavor_id: 13,
-  },
-  {
-    product_id: 2,
-    flavor_id: 8,
-  },
-  {
-    product_id: 3,
-    flavor_id: 10,
-  },
-  {
-    product_id: 4,
-    flavor_id: 7,
-  },
-  {
-    product_id: 5,
-    flavor_id: 15,
-  },
-  {
-    product_id: 6,
-    flavor_id: 2,
-  },
-];
+module.exports = () => {
+  let pairs = [];
+
+  for (let i = 0, j = i + 1; i < 2351; i++, j++) {
+    pairs.push({ product_id: j, flavor_id: j });
+  }
+
+  return pairs;
+};
