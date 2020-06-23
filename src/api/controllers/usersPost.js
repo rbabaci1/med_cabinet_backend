9e0cd47c1d76a7a43a7731b3cb852913c7991de9
rabah babaci <4rabah@gmail.com>
2020-06-22 18:26:45 -0700

    reset the database

--- src/api/controllers/usersPost.js
+++ src/api/controllers/usersPost.js
@@ -8,9 +8,10 @@ const registerUser = async (req, res) => {
     const user = { ...req.body, created_at: now() };
     const hash = bcrypt.hashSync(user.password, 10);
 
-    const insertedUser = await User.create({ ...user, password: hash });
+    const createdUser = await User.create({ ...user, password: hash });
+    // generate a token if Front End redirecting to home page after signup
 
-    res.status(201).json({ success: true, created: { ...insertedUser } });
+    res.status(201).json({ success: true, created: { ...createdUser } });
   } catch ({ message }) {
     res.status(500).json({
       message: "An error occurred while inserting to the database.",
