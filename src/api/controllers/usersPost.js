6237ca5c3ea81ccdd0337d34236172af7b90debd
rabah babaci <4rabah@gmail.com>
2020-06-23 12:38:02 -0700

    create the user login endpoint

--- src/api/controllers/usersPost.js
+++ src/api/controllers/usersPost.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const now = require("../../helpers/getLocalDateTime");
 const generateToken = require("../../helpers/generateToken.js");
 const removeObjKey = require("../../helpers/removeObjKey.js");
+const { getBy } = require("../../db/models/global");
 const { SALT_ROUNDS } = require("../../config");
 const User = require("../../db/models/users");
 
@@ -24,11 +25,27 @@ const registerUser = async (req, res) => {
   }
 };
 
-const loginUser = async (req, res) => {
+const loginUser = async (req, res, next) => {
   try {
-    res.status(200).json({ message: "made it!" });
+    const { email, password } = req.body;
+    const user = await getBy("users", { email });
+
+    if (user && bcrypt.compare(password, user.password)) {
+      const token = generateToken(user);
+      res.status(200).json({
+        message: "Welcome to best med cabinet in the world!",
+        user,
+        token,
+      });
+    } else {
+      res.status(401).json({ message: "Invalid credentials. Try again?" });
+    }
   } catch (error) {
-    console.error(error);
+    // res.status(500).json({
+    //   message: "An error occurred while authenticating the user.",
+    //   reason: message,
+    // });
+    next(error);
   }
 };
 
