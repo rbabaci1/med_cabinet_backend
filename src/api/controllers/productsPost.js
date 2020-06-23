697771dc58c594a42d64fbe149d1b2a4193aa8de
rabah babaci <4rabah@gmail.com>
2020-06-23 16:50:36 -0700

    create the getRecommendations controller

--- src/api/controllers/productsPost.js
+++ src/api/controllers/productsPost.js
@@ -3,6 +3,16 @@ const Product = require("../../db/models/products");
 
 const TABLE_NAME = "products";
 
-const getRecommendations = async (req, res, next) => {};
+const getRecommendations = async (req, res, next) => {
+  const userInfo = req.body;
+
+  const rawResponse = await fetch("https://medicabi.herokuapp.com/send", {
+    method: "POST",
+    body: userInfo,
+  });
+  const recommendations = await rawResponse.json();
+
+  console.log(recommendations);
+};
 
 module.exports = { getRecommendations };
