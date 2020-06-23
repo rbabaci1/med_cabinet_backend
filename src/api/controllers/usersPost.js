9d7030be38ad11c7fb6dc77c76f76650094bf3ed
rabah babaci <4rabah@gmail.com>
2020-06-22 17:58:03 -0700

    create the register user req handler

--- src/api/controllers/usersPost.js
+++ src/api/controllers/usersPost.js
@@ -1,11 +1,12 @@
 const User = require("../../db/models/users");
 const now = require("../../utils/getLocalDateTime");
 
-const registerUser = (req, res) => {
+const registerUser = async (req, res) => {
   try {
-    // add created_at
+    const newUser = { ...req.body, created_at: now() };
+    const insertedUser = await User.create(newUser);
 
-    res.status(200).json(req.body);
+    res.status(200).json({ success: true, created: { ...insertedUser } });
   } catch (error) {
     console.error(error);
   }
--- src/api/validationSchemas/index.js
+++ src/api/validationSchemas/index.js
@@ -6,9 +6,6 @@ const userSchema = Joi.object({
   email: Joi.string().email().required(),
   username: Joi.string().alphanum().min(3).required(),
   password: Joi.string().required(),
-  created_at: Joi.string()
-    .pattern(new RegExp("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$"))
-    .required(),
 });
 
 const dispensarySchema = Joi.object({
@@ -21,9 +18,6 @@ const dispensarySchema = Joi.object({
   email: Joi.string().email().required(),
   logo_url: Joi.string().min(10).required(),
   has_delivery: Joi.boolean().required(),
-  created_at: Joi.string()
-    .pattern(new RegExp("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$"))
-    .required(),
 });
 
 module.exports = { userSchema, dispensarySchema };
