12839d6b503748ca61e89d6b7568e66dd7c958b9
rabah babaci <4rabah@gmail.com>
2020-06-22 23:09:13 -0700

    finish with products seed insertion

--- src/db/seeds/03-products.js
+++ src/db/seeds/03-products.js
@@ -1,47 +1,36 @@
-const { generateProducts } = require("../../utils/dbSeedHelpers");
-
-const products = generateProducts();
-
-const _1 = products.slice(0, 80);
-const _2 = products.slice(80, 160);
-const _3 = products.slice(160, 240);
-const _4 = products.slice(240, 320);
-const _5 = products.slice(320, 400);
-const _6 = products.slice(400, 480);
-const _7 = products.slice(480, 560);
-const _8 = products.slice(560, 640);
-const _9 = products.slice(640, 720);
-const _10 = products.slice(720, 800);
-const _11 = products.slice(800, 880);
-const _12 = products.slice(880, 960);
-const _13 = products.slice(960, 1040);
-const _14 = products.slice(1040, 1120);
-const _15 = products.slice(1120, 1200);
-const _16 = products.slice(1200, 1280);
-const _17 = products.slice(1280, 1360);
-const _18 = products.slice(1360, 1440);
-const _19 = products.slice(1440, 1520);
-const _20 = products.slice(1520, 1600);
-const _21 = products.slice(1600, 1680);
-const _22 = products.slice(1680, 1760);
-const _23 = products.slice(1760, 1840);
-const _24 = products.slice(1840, 1920);
-const _25 = products.slice(1920, 2000);
-const _26 = products.slice(2000, 2080);
-const _27 = products.slice(2080, 2160);
-const _28 = products.slice(2160, 2240);
-const _29 = products.slice(2240, 2320);
-const _30 = products.slice(2320);
-
-// const secondHalf = products.splice(49);
+const products = require("../../utils/splitProductsSeedData");
 
 exports.seed = function (knex) {
   return knex("products")
     .del()
-    .then(function () {
-      return knex("products").insert(one);
-    });
-  // .then(function () {
-  //   return knex("products").insert(secondHalf);
-  // });
+    .then(() => knex("products").insert(products._1))
+    .then(() => knex("products").insert(products._2))
+    .then(() => knex("products").insert(products._3))
+    .then(() => knex("products").insert(products._4))
+    .then(() => knex("products").insert(products._5))
+    .then(() => knex("products").insert(products._6))
+    .then(() => knex("products").insert(products._7))
+    .then(() => knex("products").insert(products._8))
+    .then(() => knex("products").insert(products._9))
+    .then(() => knex("products").insert(products._10))
+    .then(() => knex("products").insert(products._11))
+    .then(() => knex("products").insert(products._12))
+    .then(() => knex("products").insert(products._13))
+    .then(() => knex("products").insert(products._14))
+    .then(() => knex("products").insert(products._15))
+    .then(() => knex("products").insert(products._16))
+    .then(() => knex("products").insert(products._17))
+    .then(() => knex("products").insert(products._18))
+    .then(() => knex("products").insert(products._19))
+    .then(() => knex("products").insert(products._20))
+    .then(() => knex("products").insert(products._21))
+    .then(() => knex("products").insert(products._22))
+    .then(() => knex("products").insert(products._23))
+    .then(() => knex("products").insert(products._24))
+    .then(() => knex("products").insert(products._25))
+    .then(() => knex("products").insert(products._26))
+    .then(() => knex("products").insert(products._27))
+    .then(() => knex("products").insert(products._28))
+    .then(() => knex("products").insert(products._29))
+    .then(() => knex("products").insert(products._30));
 };
