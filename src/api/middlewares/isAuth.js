856c629781b120f9206d56261998ff1499f6427c
rabah babaci <4rabah@gmail.com>
2020-06-23 14:57:39 -0700

    create the isAuth middleware

--- src/api/middlewares/isAuth.js
+++ src/api/middlewares/isAuth.js
@@ -1,4 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const { JWT_SECRET } = require("../../config");
+
 module.exports = (req, res, next) => {
   try {
-  } catch (error) {}
+    const { authorization } = req.headers;
+    const { token } = authorization ? authorization : null;
+
+    if (token) {
+      jwt.verify(token, JWT_SECRET, (error, decodedToken) => {
+        if (error) {
+          next(error);
+        } else {
+          console.log(decodedToken);
+          next();
+        }
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
 };
