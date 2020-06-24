e5531bc34c34b3d799ba046082ad79fa4d79ce08
rabah babaci <4rabah@gmail.com>
2020-06-24 13:06:01 -0700

    finish create user review endpoint

--- src/api/middlewares/validateReviewInfo.js
+++ src/api/middlewares/validateReviewInfo.js
@@ -1,11 +1,23 @@
 const { reviewSchema } = require("../validationSchemas");
 const formatError = require("../../helpers/formatError");
+const { getBy } = require("../../db/models/global");
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   const result = reviewSchema.validate(req.body);
 
   if (!result.error) {
-    next();
+    const user = await getBy("users", { id: req.body.user_id });
+    const product = await getBy("products", { id: req.body.product_id });
+
+    if (!user) {
+      res.status(401).json({ message: "The specified user_id is not valid." });
+    } else if (!product) {
+      res
+        .status(401)
+        .json({ message: "The specified product_id is not valid." });
+    } else {
+      next();
+    }
   } else {
     res.status(400).json(formatError(result.error));
   }
