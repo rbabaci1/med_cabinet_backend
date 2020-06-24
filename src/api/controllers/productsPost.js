3844f0bd038e9f9a13bdc0787650da8289403559
rabah babaci <4rabah@gmail.com>
2020-06-24 10:28:54 -0700

    fix the strain recommandations enpoint

--- src/api/controllers/productsPost.js
+++ src/api/controllers/productsPost.js
@@ -1,20 +1,22 @@
-const axios = require("axios");
+const { default: Axios } = require("axios");
 const fetch = require("node-fetch");
 
 const { getBy } = require("../../db/models/global");
-const Product = require("../../db/models/products");
 
 const TABLE_NAME = "products";
 
 const getRecommendations = async (req, res, next) => {
-  fetch("https://medicabi.herokuapp.com/send", {
-    method: "POST",
-    body: req.body,
-    headers: { "Content-Type": "application/json" },
-  })
-    .then(res => console.log(res))
-    .catch(err => next(err));
-  // .then(res => console.log(res));
+  try {
+    const data = req.body;
+
+    const response = await Axios.post(
+      "https://medicabi.herokuapp.com/send",
+      data
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = { getRecommendations };
