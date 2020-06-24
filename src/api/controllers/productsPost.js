dc1c0d58924af6e24125a1f41332d7a74fe5321b
rabah babaci <4rabah@gmail.com>
2020-06-23 17:00:13 -0700

    install axios

--- src/api/controllers/productsPost.js
+++ src/api/controllers/productsPost.js
@@ -4,15 +4,19 @@ const Product = require("../../db/models/products");
 const TABLE_NAME = "products";
 
 const getRecommendations = async (req, res, next) => {
-  const userInfo = req.body;
+  try {
+    const userInfo = req.body;
 
-  const rawResponse = await fetch("https://medicabi.herokuapp.com/send", {
-    method: "POST",
-    body: userInfo,
-  });
-  const recommendations = await rawResponse.json();
+    const rawResponse = await fetch("https://medicabi.herokuapp.com/send", {
+      method: "POST",
+      body: userInfo,
+    });
+    const recommendations = await rawResponse.json();
 
-  console.log(recommendations);
+    console.log(recommendations);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = { getRecommendations };
