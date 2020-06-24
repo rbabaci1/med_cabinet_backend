0a77a69191617d01b3ef35b7579c82567984825c
rabah babaci <4rabah@gmail.com>
2020-06-24 12:36:01 -0700

    add the validateReviewInfo to the review route

--- src/api/routes/users.js
+++ src/api/routes/users.js
@@ -1,9 +1,8 @@
-const { route } = require("./usersAuth");
-
 const router = require("express").Router();
 
 const User = require("../controllers/usersPost");
+const validateReviewInfo = require("../middlewares/validateReviewInfo");
 
-router.post("/review", User.createReview);
+router.post("/review", validateReviewInfo, User.createReview);
 
 module.exports = router;
