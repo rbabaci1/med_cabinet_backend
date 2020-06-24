f225c0195500c55447cca7f891e16dd0ab1be179
rabah babaci <4rabah@gmail.com>
2020-06-23 17:47:53 -0700

    create the createDispensary controller

--- src/api/controllers/dispensaryPost.js
+++ src/api/controllers/dispensaryPost.js
@@ -1,7 +1,13 @@
 const Dispensary = require("../../db/models/dispensaries");
 
-const createDispensary = async (req, res) => {
-  res.send("made it");
+const createDispensary = async (req, res, next) => {
+  try {
+    const createdDispensary = await Dispensary.create(req.body);
+
+    res.status(201).json({ success: true, createdDispensary });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = { createDispensary };
