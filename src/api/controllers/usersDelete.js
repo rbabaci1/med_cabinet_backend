09bff28abc98cb3640457910eae627a3b9f60dd8
rabah babaci <4rabah@gmail.com>
2020-06-24 18:56:26 -0700

    create the delete use review endpoint

--- src/api/controllers/usersDelete.js
+++ src/api/controllers/usersDelete.js
@@ -1,4 +1,4 @@
-const { remove } = require("../../db/models/global");
+const { remove, getBy } = require("../../db/models/global");
 
 const removeCartItem = async (req, res, next) => {
   try {
@@ -11,4 +11,31 @@ const removeCartItem = async (req, res, next) => {
   }
 };
 
-module.exports = { removeCartItem };
+const removeReview = async (req, res, next) => {
+  try {
+    const { product_id, user_id } = req.body;
+
+    const user = await getBy("users", { id: user_id });
+    const product = await getBy("products", { id: product_id });
+    const review = await getBy("ratings", { user_id, product_id });
+
+    if (!user) {
+      res.status(404).json({ message: "The specified user_id is not valid." });
+    } else if (!product) {
+      res
+        .status(404)
+        .json({ message: "The specified product_id is not valid." });
+    } else if (!review) {
+      res.status(404).json({
+        message: "The specified review doesn't exists .",
+      });
+    } else {
+      await remove("ratings", { product_id });
+      res.status(200).json({ message: "success", removedReview: review });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { removeCartItem, removeReview };
