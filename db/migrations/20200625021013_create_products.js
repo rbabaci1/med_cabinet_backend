c1f4243c7ddabac715a3e9b3c5cd171340531b97
rabah babaci <4rabah@gmail.com>
2020-06-25 16:24:19 -0700

    reset db

--- db/migrations/20200625021013_create_products.js
+++ db/migrations/20200625021013_create_products.js
@@ -1,19 +1,19 @@
 exports.up = function (knex) {
   return knex.schema.createTable("products", table => {
     table.increments();
-    table.string("strain_name").notNullable();
-    table.string("strain_category").notNullable();
-    table.string("strain_type").notNullable();
-    table.string("flavors").notNullable();
-    table.string("effects").notNullable();
-    table.integer("avg_thc").notNullable();
-    table.integer("avg_cbd").notNullable();
+    table.string("strain_name", 64).notNullable();
+    table.string("strain_category", 64).notNullable();
+    table.string("strain_type", 64).notNullable();
+    table.string("flavors", 128).notNullable();
+    table.string("effects", 128).notNullable();
+    table.float("avg_thc").notNullable();
+    table.float("avg_cbd").notNullable();
     table.integer("price").notNullable();
-    table.string("price_unit").notNullable();
+    table.string("price_unit", 64).notNullable();
     table.string("description").notNullable();
-    table.string("img_url").notNullable();
+    table.string("img_url", 128).notNullable();
     table.boolean("is_available").notNullable();
-    table.string("created_at").notNullable();
+    table.string("created_at", 64).notNullable();
     table
       .integer("dispensary_id")
       .unsigned()
