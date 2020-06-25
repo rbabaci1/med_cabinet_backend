adae4d9e9332849aa124ac5b6c441909ec3682e3
rabah babaci <4rabah@gmail.com>
2020-06-25 13:06:33 -0700

    create heroku app

--- src/knexfile.js
+++ src/knexfile.js
@@ -1,14 +1,14 @@
 const path = require("path");
 
 // const { pgConnection } = require("./config");
-const pgConnection = process.env.DATABASE_URL;
+// const pgConnection = process.env.DATABASE_URL;
 
 module.exports = {
   development: {
     client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: "./db/med_cabinet.db",
+      filename: path.resolve(__dirname, "./db/med-cabinet.db"),
     },
     migrations: {
       directory: "./db/migrations",
