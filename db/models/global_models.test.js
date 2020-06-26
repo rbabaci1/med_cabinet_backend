253de34804615c7a6dde567c0b933a3b6fb7dfc2
rabah babaci <4rabah@gmail.com>
2020-06-25 17:52:11 -0700

    add a test for the update db model

--- db/models/global_models.test.js
+++ db/models/global_models.test.js
@@ -1,26 +1,51 @@
 const db = require("../dbConfig");
 const { getAll, getBy, update, remove } = require("./global");
 
+const mockUser = {
+  firstName: "rabah",
+  lastName: "babaci",
+  email: "hello@gmail.com",
+  password: "hello",
+  created_at: "2020-06-23 11:41:40",
+};
+
 describe("global db models", () => {
   describe("getAll()", () => {
-    it("should return all entities of the specified tableName", async () => {
-      const original = await db("users");
-      const users = await getAll("users");
+    beforeEach(async () => {
+      await db("users").truncate();
+      await db("users").insert(mockUser);
+    });
 
-      expect(original).toEqual(users);
-      expect(original.length).toEqual(users.length);
+    it("returns all entities of the specified tableName", async () => {
+      const [user] = await getAll("users");
+
+      expect(user).toEqual({ id: 1, ...mockUser });
+      expect(user.lastName).toBe("babaci");
     });
   });
 
   describe("getBy()", () => {
-    it("should return a single entity of the specified tableName and using the filter passed", async () => {
-      const original = await db("products").where({ id: 10 }).first();
-      const product1 = await getBy("products", { id: 10 });
-      const product2 = await getBy("products", { id: 99 });
+    it("returns a single entity of the specified tableName via the filter passed", async () => {
+      const user1 = await getBy("users", { id: 1 });
+      const user2 = await getBy("users", { id: 2 });
 
-      expect(original).toEqual(product1);
-      expect(original.id).toEqual(product1.id);
-      expect(product2.id).toBe(99);
+      expect(user1.firstName).toBe("rabah");
+      expect(user2).toBeFalsy();
+    });
+  });
+
+  describe("update()", () => {
+    it("updates an entity of the specified tableName with the provided changes via the filter", async () => {
+      const changes = {
+        firstName: "james",
+        lastName: "bond",
+        email: "james@bond.com",
+      };
+
+      await update("users", changes, { id: 1 });
+      const updatedUser = await db("users").where({ id: 1 }).first();
+
+      expect(updatedUser.email).toBe("james@bond.com");
     });
   });
 });
