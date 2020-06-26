7ff6f34b88256271eddf6179874f5df4bbb51d3b
rabah babaci <4rabah@gmail.com>
2020-06-25 19:15:23 -0700

    define the getProducts() db model test

--- db/models/users.test.js
+++ db/models/users.test.js
@@ -2,5 +2,7 @@ const db = require("../dbConfig");
 const User = require("./users");
 
 describe("users.js db models", () => {
-  it("", () => {});
+  describe("getProducts()", () => {
+    it("returns all the products of a specified user", async () => {});
+  });
 });
