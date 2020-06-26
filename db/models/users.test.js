4a5c0317fc2c5b3eeeca828b8e41d92ea33561b3
rabah babaci <4rabah@gmail.com>
2020-06-25 19:39:23 -0700

    create a test get user products db model

--- db/models/users.test.js
+++ db/models/users.test.js
@@ -4,9 +4,16 @@ const User = require("./users");
 describe("users.js db models", () => {
   describe("getProducts()", () => {
     it("returns all the products of a specified user via user id", async () => {
-      const products = await User.getProducts(1);
+      const products = await User.getProducts(3);
 
-      console.log(products);
+      const origin = await db("users_carts as u_c")
+        .join("users as u", "u_c.user_id", "u.id")
+        .join("products as p", "u_c.product_id", "p.id")
+        .select("p.*")
+        .where({ user_id: 3 });
+
+      expect(products.length).toEqual(origin.length);
+      expect(products[0].strain_name).toEqual(origin[0].strain_name);
     });
   });
 });
