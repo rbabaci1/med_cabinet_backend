c57c20a7342c90da3057c7c7ca5ac6faf25d93ae
rabah babaci <4rabah@gmail.com>
2020-06-26 18:32:12 -0700

    add testes for user authentication routes

--- api/controllers/users.endpoints.test.js
+++ api/controllers/users.endpoints.test.js
@@ -1,36 +1,79 @@
 const supertest = require("supertest");
 
 const cleanUpDatabase = require("../../helpers/cleanUpDatabase");
-const now = require("../../helpers/getLocalDateTime");
 const server = require("../server");
 const request = supertest(server);
 
-afterEach(() => cleanUpDatabase());
+let mockUser = {
+  firstName: "rabah",
+  lastName: "babaci",
+  email: "test@gmail.com",
+};
 
 describe("users endpoints", () => {
-  describe("create new user", () => {
-    it("should respond with 400 and a message with password is required", async () => {
-      const res = await request.post("/api/auth/register").send({
-        firstName: "rabah",
-        lastName: "babaci",
-        email: "rbabaci1@gmai.com",
-      });
+  /*******************************   SIGNUP   **********************************/
+  describe("User sign up", () => {
+    it("should respond with 400 and a message saying password is required", async () => {
+      const res = await request.post("/api/auth/register").send(mockUser);
 
       expect(res.status).toBe(400);
       expect(res.body.message).toBe(`"password" is required`);
     });
 
-    it("should respond with 400 and a message with password is required", async () => {
+    it("should respond with 201 and return the created user without the password", async () => {
       const res = await request.post("/api/auth/register").send({
-        firstName: "rabah",
-        lastName: "babaci",
-        email: "rbabaci1@gmai.com",
-        password: "hello there",
+        ...mockUser,
+        password: "correct password",
       });
 
       expect(res.status).toBe(201);
       expect(res.body).toHaveProperty("createdUser");
       expect(res.body.createdUser).not.toHaveProperty("password");
+    });
+
+    it("should respond with 409", async () => {
+      const res = await request.post("/api/auth/register").send({
+        ...mockUser,
+        password: "correct password",
+      });
+
+      expect(res.status).toBe(409);
+      expect(res.body).toHaveProperty("success", false);
+    });
+  });
+
+  /*******************************   LOGIN   **********************************/
+  describe("User login", () => {
+    it("should respond with 400 and a message saying email is required", async () => {
+      const res = await request.post("/api/auth/login").send({
+        password: "hello there",
+      });
+
+      expect(res.status).toBe(400);
+      expect(res.body.message).toBe(`"email" is required`);
+    });
+
+    it("should respond with 401 and a message", async () => {
+      const res = await request.post("/api/auth/login").send({
+        email: "test@gmail.com",
+        password: "wrong password",
+      });
+
+      expect(res.status).toBe(401);
+      expect(res.body).toHaveProperty("message");
+    });
+
+    it("should respond with 200 and the logged user info", async () => {
+      const res = await request.post("/api/auth/login").send({
+        email: "test@gmail.com",
+        password: "correct password",
+      });
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("logged_user");
+      expect(res.body).toHaveProperty("token");
+
+      cleanUpDatabase();
     });
   });
 });
