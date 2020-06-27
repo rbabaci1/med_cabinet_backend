7f7cc3b38c4cefba695476caa5f99dcec19a09a5
rabah babaci <4rabah@gmail.com>
2020-06-26 19:29:10 -0700

    reset local database

--- helpers/generateProductsSeed.js
+++ helpers/generateProductsSeed.js
@@ -6,7 +6,8 @@ const {
 } = require("../db/mockData/products");
 
 const random1 = arr => arr[Math.floor(Math.random() * arr.length)];
-const random2 = (min, max) =>
+const random2 = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const random3 = (min, max) =>
   Math.floor(Math.random() * (max - min + 1)) + min / 100;
 
 const generateProducts = () => {
@@ -14,10 +15,10 @@ const generateProducts = () => {
     p.strain_category = random1(strain_categories) || strain_categories[0];
     p.avg_thc = random2(5, 40);
     p.avg_cbd = random2(5, 40);
-    p.price = random2(5, 50);
-    p.created_at = "2019-10-17 08:11:20";
     p.dispensary_id = random2(1, 30);
+    p.price = random3(5, 50);
     p.description = p.description.slice(0, 255);
+    p.created_at = "2019-10-17 08:11:20";
 
     p.img_url =
       p.strain_category === "Flower"
